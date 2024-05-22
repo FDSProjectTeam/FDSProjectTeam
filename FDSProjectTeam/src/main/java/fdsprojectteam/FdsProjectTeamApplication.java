5039f5aebb9ea496faa6f2218e0985bb5dc02a35
wndudtjd <dudtjdwn1575@naver.com>
2024-05-22 16:10:49 +0900

    FDS프로젝트 생성

---
+++ FDSProjectTeam/src/main/java/fdsprojectteam/FdsProjectTeamApplication.java
@@ -0,0 +1,20 @@
+package fdsprojectteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class FdsProjectTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdsProjectTeamApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "thymeleaf/index";
+    }
+}
