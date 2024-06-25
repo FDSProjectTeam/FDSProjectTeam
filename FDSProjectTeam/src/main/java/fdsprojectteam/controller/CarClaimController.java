6f99344106abeca92ea5570ca9dfa0d17748572e
kimyonghyuk <gur0103@gmail.com>
2024-06-25 14:29:04 +0900

    [FEAT] 자동차 보험 Chart.js 추가

--- FDSProjectTeam/src/main/java/fdsprojectteam/controller/CarClaimController.java
+++ FDSProjectTeam/src/main/java/fdsprojectteam/controller/CarClaimController.java
@@ -9,15 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import fdsprojectteam.command.CarClaimCommand;
+import fdsprojectteam.mapper.CarClaimMapper;
+import fdsprojectteam.service.carclaim.CarClaimAutonumService;
+import fdsprojectteam.service.carclaim.CarClaimChartService;
 import fdsprojectteam.service.carclaim.CarClaimWriteService;
 
 @Controller
 public class CarClaimController {
 	@Autowired
 	CarClaimWriteService carClaimWriteService;
-	
+    @Autowired
+    CarClaimAutonumService carClaimAutonumService;
 	@GetMapping("carClaim")
-	public String carClaimForm(Model model) {
+	public String carClaimForm(CarClaimCommand carClaimCommand, Model model) {
+		carClaimAutonumService.execute(model);
 		return "thymeleaf/carClaim/carClaimForm";
 	}
     @PostMapping("carClaimWrite")
@@ -25,7 +30,14 @@ public class CarClaimController {
         List<String> resultMessages = carClaimWriteService.execute(carClaimCommand, model);
         model.addAttribute("resultMessages", resultMessages);
      // 클레임 처리 결과 설정
-
         return "thymeleaf/carClaim/carClaimResult";
 	}
+    @Autowired
+    CarClaimChartService carClaimChartService;
+    @GetMapping("carClaimChart")
+    public String carClaimChart(Model model) {
+        List<CarClaimCommand> chartData = carClaimChartService.getChartData(); 
+        model.addAttribute("chartData", chartData); 
+        return "thymeleaf/carClaim/carClaimChart"; 
+    }
 }
